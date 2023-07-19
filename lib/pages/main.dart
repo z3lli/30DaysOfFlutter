56803ac13e6c848315fd04c7e854062bdb49c871
Usama <usamajaffar2018@gmail.com>
2023-07-20 03:42:24 +0500

    day3 compelete

---
+++ lib/pages/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:pubguc/pages/home_page.dart';
+import 'package:pubguc/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+}
