de2d6a6dfecf9d60055e9bb601448864cc1dc9a7
Usama <usamajaffar2018@gmail.com>
2023-07-20 16:51:51 +0500

    day4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,12 +6,51 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
-        ),
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/drag.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "PUBG UC SHOP",
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, color: Colors.red),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "USER", hintText: "Enter UserName"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: "PASS", hintText: "Enter Password"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: (() {
+                    print("Hellow world ;)");
+                  }),
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(foregroundColor: Colors.white),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
