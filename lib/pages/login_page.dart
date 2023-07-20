1ed8089c696c857731b6cfd5266db7be64c760a6
Usama <usamajaffar2018@gmail.com>
2023-07-20 18:15:52 +0500

    day5 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pubguc/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,51 +7,55 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/drag.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "PUBG UC SHOP",
-            style: TextStyle(
-                fontSize: 24, fontWeight: FontWeight.bold, color: Colors.red),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "USER", hintText: "Enter UserName"),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: "PASS", hintText: "Enter Password"),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: (() {
-                    print("Hellow world ;)");
-                  }),
-                  child: Text("Login"),
-                  style: TextButton.styleFrom(foregroundColor: Colors.white),
-                )
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/drag.png",
+              fit: BoxFit.cover,
             ),
-          )
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "PUBG UC SHOP",
+              style: TextStyle(
+                  fontSize: 24, fontWeight: FontWeight.bold, color: Colors.red),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "USER", hintText: "Enter UserName"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "PASS", hintText: "Enter Password"),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  ElevatedButton(
+                    onPressed: (() {
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    }),
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        minimumSize: Size(150, 40)),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pubguc/pages/home_page.dart';
 import 'package:pubguc/pages/login_page.dart';
+import 'package:pubguc/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,8 +24,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
