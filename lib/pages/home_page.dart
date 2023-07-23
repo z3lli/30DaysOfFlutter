9109e7220246921472a433a75c72aa47d975a9e0
Usama <usamajaffar2018@gmail.com>
2023-07-23 05:00:49 +0500

    day13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pubguc/models/catalog.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
+import 'dart:convert';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var cataLogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decoderData = jsonDecode(cataLogJson);
+    print(decoderData);
+  }
 
   @override
   Widget build(BuildContext context) {
